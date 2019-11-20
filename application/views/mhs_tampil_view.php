265392429cbae1df748eb2ef7206cd1f98af77fa
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-20 11:30:38 +0700

    first commit

---
+++ application/views/mhs_tampil_view.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+	<div id="container">
+		<h1>Tampil Mahasiswa</h1>
+
+		<div id="row">
+			<p>Menampilkan Data Mahasiswa</p>
+		</div>
+
+		<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	</div>
+
+</body>
+
+</html>
