2ea5c444b668b6e06c9fc39d539954f252ee36ba
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-21 10:57:40 +0700

    menambahkan update

--- application/controllers/api/Student.php
+++ application/controllers/api/Student.php
@@ -6,7 +6,7 @@ require APPPATH . 'libraries/REST_Controller.php';
 
 class Student extends REST_Controller
 {
-    public function index_get()
+    /*public function index_get()
     {
         $nim = $this->get('nim');
         if ($nim == '') {
@@ -18,16 +18,31 @@ class Student extends REST_Controller
         }
 
         return $this->response($data, 200);
+    }*/
+
+    public function index_get(){
+        $nim = $this->get('nim');
+        if($nim==''){
+            $this->db->order_by('nama','ASC');
+            $data = $this->db->get('Student')->result();
+        }
+        else {
+            $this->db->where('nim',$nim);
+            $data = $this->db->get('Student')->result();
+        }
+        return $this->response($data);
     }
 
     public function getbyname_get($nama)
     {
-        $sql = "select * from Student where nama like ?";
-        $data = $this->db->query($sql, array('%' . $nama . '%'))->result();
+        /*$sql = "select * from Student where nama like ?";
+        $data = $this->db->query($sql, array('%' . $nama . '%'))->result();*/
+        $this->db->like('nama',$nama);
+        $data = $this->db->get('Student')->result();
         return $this->response($data, 200);
     }
 
-    public function index_post()
+    /*public function index_post()
     {
         $nim = $this->post('nim');
         $nama = $this->post('nama');
@@ -43,17 +58,40 @@ class Student extends REST_Controller
         } else {
             return $this->response("Gagal Insert Data !", 400);
         }
+    }*/
+
+    public function index_post(){
+        $nim = $this->post('nim');
+        $nama = $this->post('nama');
+        $email = $this->post('email');
+        $ipk = $this->post('ipk');
+
+        $data = ['nim' => $nim, 'nama' => $nama, 'email' => $email, 'ipk' => $ipk];
+        $result = $this->db->insert('Student',$data);
+        if($result){
+            return $this->response($data,201);
+        }
+        else {
+            return $this->response("Gagal insert data !",400);
+        }
     }
 
     public function index_put()
     {
         $nim = $this->put('nim');
         $nama = $this->put('nama');
+        $email = $this->put('email');
         $ipk = $this->put('ipk');
 
-        $data = ['nim' => $nim, 'nama' => $nama, 'ipk' => $ipk];
-
-        return $this->response($data, 200);
+        $sql = "update Student set nama=?,email=?,ipk=? where nim=?";
+        $result = $this->db->query($sql,array($nama,$email,$ipk,$nim));
+        if($result){
+            return $this->response("Data berhasil diupdate",200);
+        }
+        else {
+            return $this->response("Data gagal diupdate",400);
+        }
+        
     }
 
     public function index_delete($nim)
