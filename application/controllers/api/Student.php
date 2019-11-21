bd9074ef0f8cb0676ab3d403751cefb6a19f0248
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-21 10:27:07 +0700

    menambahkan insert data

--- application/controllers/api/Student.php
+++ application/controllers/api/Student.php
@@ -1,65 +1,63 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . 'libraries/REST_Controller.php';
 
-class Student extends REST_Controller 
+class Student extends REST_Controller
 {
-    public function index_get(){
+    public function index_get()
+    {
         $nim = $this->get('nim');
-        if($nim==''){
+        if ($nim == '') {
             $sql = "select * from Student order by nama";
             $data = $this->db->query($sql)->result();
-        }
-        else {
+        } else {
             $sql = "select * from Student where nim=?";
-            $data = $this->db->query($sql,array($nim))->result()[0];
+            $data = $this->db->query($sql, array($nim))->result()[0];
         }
-        
-        return $this->response($data,200);
+
+        return $this->response($data, 200);
     }
 
-
-    public function showparam_get(){
-        $myId = $this->get('id');
-        $myName = $this->get('name');
-        $data = ['id'=>$myId,'name'=>$myName];
-        return $this->response($data,200);
+    public function getbyname_get($nama)
+    {
+        $sql = "select * from Student where nama like ?";
+        $data = $this->db->query($sql, array('%' . $nama . '%'))->result();
+        return $this->response($data, 200);
     }
 
-    public function index_post(){
+    public function index_post()
+    {
         $nim = $this->post('nim');
         $nama = $this->post('nama');
+        $email = $this->post('email');
         $ipk = $this->post('ipk');
 
-        $data = array('nim'=>$nim,'nama'=>$nama,'ipk'=>$ipk);
+        $data = ['nim' => $nim, 'nama' => $nama, 'email' => $email, 'ipk' => $ipk];
+        $sql = "insert into Student(nim,nama,email,ipk) values(?,?,?,?)";
 
-        return $this->response($data,200);
+        $result = $this->db->query($sql, array($nim, $nama, $email, $ipk));
+        if ($result) {
+            return $this->response($data, 201);
+        } else {
+            return $this->response("Gagal Insert Data !", 400);
+        }
     }
 
-    public function hitungsegitiga_post(){
-        $alas = $this->post("alas");
-        $tinggi = $this->post("tinggi");
-
-        $hasil = 0.5*$alas*$tinggi;
-
-        return $this->response("Hasil: $hasil",200);
-    }
-
-    public function index_put(){
+    public function index_put()
+    {
         $nim = $this->put('nim');
         $nama = $this->put('nama');
         $ipk = $this->put('ipk');
 
-        $data = ['nim'=>$nim,'nama'=>$nama,'ipk'=>$ipk];
+        $data = ['nim' => $nim, 'nama' => $nama, 'ipk' => $ipk];
 
-        return $this->response($data,200);
+        return $this->response($data, 200);
     }
 
-    public function index_delete($nim){
-        return $this->response("Data $nim berhasil di delete",200);
+    public function index_delete($nim)
+    {
+        return $this->response("Data $nim berhasil di delete", 200);
     }
 }
-
-?>
