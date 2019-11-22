494afddd617cadfada2de6a03857ac9d0b4eecf8
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-22 10:46:13 +0700

    menambahkan users model

--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -4,7 +4,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User_Model extends CI_Model{
 
-    
+    public function login($username,$password){
+        $condition = ['username'=>$username,'password'=>$password];
+        $user = $this->db->get_where('users',$condition)->row();
+        return $user;
+    }
 
 }
 
